36734954cdef7de44bca3008d292ba67cc2f1e80
LeeTaeHeon <forever296@naver.com>
2024-08-13 17:13:46 +0900

    feat: 헤더 컴포넌트 생성 및 routes 객체 value 변경

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import createRouter from './router.js';
+import { Header } from './components/Header.js';
 
 //라우터 관련 코드 작성
 const routes = {
   '/': () => {
     console.log('메인 화면입니다.');
+    return '<h2>메인 화면</h2>';
   },
   '/mypage': () => {
     console.log('마이페이지 입니다.');
+    return '<h2>마이페이지</h2>';
   },
   '/404': () => {
     console.log('찾을 수 없는 페이지 입니다.');
+    return '<h2>404 - 페이지를 찾을 수 없습니다.</h2>';
   },
 };
-
 const router = createRouter(routes);
 
 const render = (content) => {
@@ -22,12 +25,27 @@ const render = (content) => {
 
 const renderPage = (path) => {
   const viewFunction = router.checkRoutes(path);
-  render(viewFunction());
+  const isDefinedRoute = routes.hasOwnProperty(path);
+
+  const content = isDefinedRoute
+    ? `
+    ${Header()}
+    ${viewFunction()}
+    `
+    : viewFunction();
+  render(content);
 };
 
 const App = () => {
   document.addEventListener('DOMContentLoaded', () => {
     renderPage(window.location.pathname);
+
+    document.body.addEventListener('click', (e) => {
+      if (e.target.matches('[data-link]')) {
+        e.preventDefault();
+        router.navigateTo(e.target.href);
+      }
+    });
   });
 
   window.addEventListener('popstate', () => {
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return `
+    <header>
+      <h1>토스 테크 블로그 SPA</h1>
+      <nav>
+        <a href="/" data-link>Home</a>
+        <a href="/mypage" data-link>My Page</a>
+      </nav>
+    </header>
+  `;
+};
