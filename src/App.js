f2528065a7ecb186a38264cd78d54c2167d1c995
LeeTaeHeon <forever296@naver.com>
2024-08-13 20:56:40 +0900

    chore: prettier 설정 변경

--- src/App.js
+++ src/App.js
@@ -7,48 +7,48 @@ import { NotFoundPage } from './pages/NotFoundPage.js';
 
 //라우터 관련 코드 작성
 const routes = {
-  '/': () => HomePage('/'),
-  '/development': () => HomePage('/development'),
-  '/design': () => HomePage('/design'),
-  '/mypage': MyPage,
-  '/404': NotFoundPage,
+    '/': () => HomePage('/'),
+    '/development': () => HomePage('/development'),
+    '/design': () => HomePage('/design'),
+    '/mypage': MyPage,
+    '/404': NotFoundPage,
 };
 
 const router = createRouter(routes);
 
 const render = (content) => {
-  const container = document.getElementById('app');
-  container.innerHTML = content;
+    const container = document.getElementById('app');
+    container.innerHTML = content;
 };
 
 const renderPage = (path) => {
-  const viewFunction = router.checkRoutes(path);
-  const isDefinedRoute = routes.hasOwnProperty(path);
+    const viewFunction = router.checkRoutes(path);
+    const isDefinedRoute = routes.hasOwnProperty(path);
 
-  const content = isDefinedRoute
-    ? `
+    const content = isDefinedRoute
+        ? `
     ${Header()}
     ${viewFunction()}
     ${Footer()}`
-    : viewFunction();
-  render(content);
+        : viewFunction();
+    render(content);
 };
 
 const App = () => {
-  document.addEventListener('DOMContentLoaded', () => {
-    renderPage(window.location.pathname);
+    document.addEventListener('DOMContentLoaded', () => {
+        renderPage(window.location.pathname);
 
-    document.body.addEventListener('click', (e) => {
-      if (e.target.matches('[data-link]')) {
-        e.preventDefault();
-        router.navigateTo(e.target.href);
-      }
+        document.body.addEventListener('click', (e) => {
+            if (e.target.matches('[data-link]')) {
+                e.preventDefault();
+                router.navigateTo(e.target.href);
+            }
+        });
     });
-  });
 
-  window.addEventListener('popstate', () => {
-    renderPage(window.location.pathname);
-  });
+    window.addEventListener('popstate', () => {
+        renderPage(window.location.pathname);
+    });
 };
 
 App();
