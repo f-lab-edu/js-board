a3baa900ddbab7e650b86d37c0d9cb440deb93e4
LeeTaeHeon <forever296@naver.com>
2024-08-20 02:43:00 +0900

    feat: 더미데이터로 게시글 데이터 생성

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,27 +1,57 @@
 import '../../styles/HomePage.css';
 import { Layout } from '../components/Layout';
 import { Tabs } from '../components/Tabs';
+import { developmentBoardData, designBoardData } from '../util/dummyData';
 
 export const HomePage = (activeTab) => {
-  const leftContent = `
-    ${Tabs(activeTab)}
-    <div class="homeContent">
-      <p>${
-        activeTab === '/'
-          ? '전체 게시글이 여기에 표시됩니다.'
-          : activeTab === '/development'
-            ? '개발 관련 게시글이 여기에 표시됩니다.'
-            : '디자인 관련 게시글이 여기에 표시됩니다.'
-      }</p>
-    </div>
-  `;
-  const rightContent = `
+    const getPosts = () => {
+        if (activeTab === '/development') {
+            return developmentBoardData;
+        } else if (activeTab === '/design') {
+            return designBoardData;
+        } else {
+            return [...developmentBoardData, ...designBoardData];
+        }
+    };
+    const posts = getPosts();
+    const postList = posts
+        .map(
+            (post) => `
+      <div class="post">
+      <div>
+      <h3>${post.title}</h3>
+      <p>${post.description}</p>
+      <small>작성자: ${post.author} | 작성일 : ${post.date}</small>
+      </div>
+      <div class="imgDiv">
+      <image src=${post.thumbnail} alt="썸네일"/>
+      </div>
+      </div>
+    `
+        )
+        .join(' ');
+
+    const messageMap = {
+        '/': '전체 게시글이 여기에 표시됩니다.',
+        '/development': '개발 관련 게시글이 여기에 표시됩니다.',
+    };
+
+    const message = messageMap[activeTab] ?? '디자인 관련 게시글이 여기에 표시됩니다.';
+
+    const leftContent = `
+        ${Tabs(activeTab)}
+        <div class="homeContent">
+          <p>${message}</p>
+          ${postList}
+        </div>
+      `;
+    const rightContent = `
   <div class="homeSidebar">
     <p>여기에 사이드바 내용이 옵니다.</p>
   </div>
 `;
 
-  return `
+    return `
     <div class="homeContainer">
     <div>
     <h2>메인 페이지</h2>
---
+++ src/util/dummyData.js
@@ -0,0 +1,50 @@
+export const developmentBoardData = [
+    {
+        id: '1',
+        title: 'Node.js 비동기 처리 성능 최적화 방법',
+        description: '비동기 처리를 최적화하여 Node.js 애플리케이션의 성능을 향상시키는 방법에 대해 논의합니다.',
+        date: '2024년 8월 20일',
+        author: '이수민',
+        board: 'development',
+        thumbnail: 'https://static.toss.im/illusts-content/tech-0812-cover3.png',
+    },
+    {
+        id: '2',
+        title: 'React 컴포넌트 재사용성 극대화',
+        description: 'React에서 컴포넌트를 재사용하기 위한 최적의 패턴과 기법들을 소개합니다.',
+        date: '2024년 8월 18일',
+        author: '박준영',
+        board: 'development',
+        thumbnail: 'https://static.toss.im/illusts-content/tech-0812-cover3.png',
+    },
+    {
+        id: '3',
+        title: 'Python 데이터 분석 자동화 도구 만들기',
+        description: 'Python을 활용한 데이터 분석 작업을 자동화하는 도구 개발에 대한 경험을 공유합니다.',
+        date: '2024년 8월 27일',
+        author: '장미희',
+        board: 'development',
+        thumbnail: 'https://static.toss.im/illusts-content/tech-0812-cover3.png',
+    },
+];
+
+export const designBoardData = [
+    {
+        id: '1',
+        title: '웹사이트 UI 디자인에서의 색상 이론 적용',
+        description: '효과적인 웹사이트 UI 디자인을 위해 색상 이론을 어떻게 적용할 수 있는지에 대해 설명합니다.',
+        date: '2024년 8월 22일',
+        author: '김혜진',
+        board: 'design',
+        thumbnail: 'https://static.toss.im/illusts-content/tech-0812-cover3.png',
+    },
+    {
+        id: '2',
+        title: 'CSS 그리드 레이아웃을 활용한 반응형 디자인',
+        description: 'CSS 그리드 레이아웃을 활용하여 반응형 웹 디자인을 손쉽게 구현하는 방법을 알아봅니다.',
+        date: '2024년 8월 25일',
+        author: '최민호',
+        board: 'design',
+        thumbnail: 'https://static.toss.im/illusts-content/tech-0812-cover3.png',
+    },
+];
