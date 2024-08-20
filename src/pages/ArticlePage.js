c205762c668f248e9fa2462199367eb4099c655d
LeeTaeHeon <forever296@naver.com>
2024-08-20 16:10:27 +0900

    design: 게시글 상세페이지 스타일 작성

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,37 @@
+import { developmentBoardData, designBoardData } from '../util/dummyData';
+import '../../styles/ArticlePage.css';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+
 export const ArticlePage = (id) => {
-    console.log(id);
-    return `
+    const allPosts = [...developmentBoardData, ...designBoardData];
+    const post = allPosts.find((post) => post.id === id);
+    console.log(post);
+    if (!post) {
+        // id에 해당하는 게시글이 없을 경우
+        return `
+        ${Header()}
     <section>
-      <h2>게시글 페이지</h2>
-      <p>${id}</p>
-    </section>
-  `;
+        <h2>게시글을 찾을 수 없습니다.</h2>
+        <p>유효하지 않은 게시글 ID입니다.</p>
+    </section>${Footer()}
+    `;
+    }
+
+    return `${Header()}
+    <div class="article-container">
+    <img src="${post.thumbnail}" alt="썸네일" class="article-image">
+
+    <h3>${post.title}</h3>
+
+    <div class="article-meta">
+        <small>${post.author}</small>
+    </div>
+
+    <p>${post.description}</p>
+
+    <div class="article-footer">
+        <span>${post.date}</span>
+    </div>
+</div>${Footer()}`;
 };
